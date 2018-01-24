76df40031e616804f4dcf71c05001a409f25305e
Akyuna Akish <akyunaakish@gmail.com>
2018-01-24 15:28:57 -0700

    finished tests for inbox smart contract

--- inbox_node_project/test/Inbox.test.js
+++ inbox_node_project/test/Inbox.test.js
@@ -14,13 +14,15 @@ const Web3 = require('web3');
 // ganache automatically creates
 // test accounts whenever it's test network
 // loads up
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 const { interface, bytecode } = require('../compile');
 
 describe('Inbox Smart Contract', () => {
     let accounts;
     let inbox;
+    const initialMessage = 'Initial Message';
 
     beforeEach(async () => {
         // an async/await function will return a promise
@@ -36,20 +38,45 @@ describe('Inbox Smart Contract', () => {
         // interface is a stringified obj, hence JSON.parse
         // many accounts are given by ganache, though
         // we just chose to use the first one arbitrarily 
+        // the send method actually sends a transaction to the network
+        // which completes the deployment process
         inbox = await new web3.eth.Contract(JSON.parse(interface))
                                   .deploy({ 
                                       data: bytecode, 
-                                      arguments: [ 'Initial Message' ] 
+                                      arguments: [ initialMessage ]
                                   })
                                   .send({ 
                                       from: accounts[0], 
                                       gas: '1000000' 
                                   });
+              
+        inbox.setProvider(provider);                          
     });
 
-    it('Deploys a contract', (done) => {
-        console.log('accs', accounts);
-        console.log('inbox', inbox);
-        done();
+    it('Deploys a contract that has an address', () => {
+        // assert.ok checks if a value is defined 'truthy'
+        assert.ok(inbox.options.address);
+    });
+
+    it('Has a default message', async () => {
+        // args for the message func can be passed into message()
+        // args for transaction details gas/gasLimit/etc can
+        // be passed into call()
+        const message = await inbox.methods.message().call();
+        assert.equal(message, initialMessage);
+    });
+
+    it('Can change the message', async () => {
+        const newMessage = 'Hi There!';
+
+        // .send({}) sends the transaction to the 
+        // ethereum network and requires an account 
+        // address to be responsible for the cost of gas
+        await inbox.methods.setMessage(newMessage).send({ 
+                                                           from: accounts[0] 
+                                                        });
+
+        const message = await inbox.methods.message().call();
+        assert.equal(message, newMessage);
     });
 });
