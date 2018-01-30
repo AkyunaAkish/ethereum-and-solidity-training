b47426a5780e42a8fe6f1e5955aa1d9cb0ddee84
Akyuna Akish <akyunaakish@gmail.com>
2018-01-30 10:16:11 -0700

    added comment

--- inbox_node_project/compile.js
+++ inbox_node_project/compile.js
@@ -5,4 +5,7 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
+// the : before Inbox is there because if there's multiple
+// Inbox contracts in different files, the filename for each
+// contract would go before the : to differentiate the contracts
 module.exports = solc.compile(source, 1).contracts[':Inbox'];
